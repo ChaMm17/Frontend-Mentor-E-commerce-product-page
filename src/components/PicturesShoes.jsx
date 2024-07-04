22b7ad1d795eb25d88f6cca1cdda4fde3284528b
ChachaMm17 <charlotte.morvant@gmail.com>
2024-07-04 21:31:12 +0200

    Change photos on click

--- src/components/PicturesShoes.jsx
+++ src/components/PicturesShoes.jsx
@@ -68,6 +68,7 @@ function PicturesShoes() {
           <ul>
             <li>
               <img
+                onClick={() => setPictureIndex([0])}
                 className="picture1"
                 src="https://img01.ztat.net/article/spp-media-p1/6ffb430c75684971bedcb5780812b39b/250d192186204b40af5a3dde6b8638fa.jpg?imwidth=1800&filter=packshot"
                 alt="sneakers 1"
@@ -75,6 +76,7 @@ function PicturesShoes() {
             </li>
             <li>
               <img
+                onClick={() => setPictureIndex([1])}
                 className="picture2"
                 src="https://img01.ztat.net/article/spp-media-p1/9493dcbd4ba34f248970c71e23bdf411/31056d77f6334644a49e3328684b0bd0.jpg?imwidth=1800"
                 alt="sneakers 2"
@@ -82,6 +84,7 @@ function PicturesShoes() {
             </li>
             <li>
               <img
+                onClick={() => setPictureIndex([2])}
                 className="picture3"
                 src="https://img01.ztat.net/article/spp-media-p1/d1af88562d56401badd0c6bbe1cdc82b/6ba23cbb61154a3997544bec8543a059.jpg?imwidth=1800"
                 alt="sneakers 3"
@@ -89,6 +92,7 @@ function PicturesShoes() {
             </li>
             <li>
               <img
+                onClick={() => setPictureIndex([3])}
                 className="picture 4"
                 src="https://img01.ztat.net/article/spp-media-p1/0c139df2194a402794d654ab29627455/bf96e816d9eb49748fe7491d5f597468.jpg?imwidth=1800"
                 alt="sneakers 4"
