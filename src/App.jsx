3cd8f9ebeda15ff50dc7b72188174f2acaeba33c
ChachaMm17 <charlotte.morvant@gmail.com>
2024-07-04 21:41:02 +0200

    creation of a new component for improved code reading

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,19 @@
 import "./styles/App.css";
 import "./styles/navbar.css";
 import "./styles/picturesshoes.css";
-
-import { useState } from "react";
+import "./styles/descriptionshoes.css";
 
 import NavBar from "./components/NavBar";
 import PicturesShoes from "./components/PicturesShoes";
+import DescriptionShoes from "./components/DescriptionShoes";
 
 function App() {
-  const [pictureIndex, setPictureIndex] = useState(0);
-
-  const picturesList = [
-    {
-      imgSrc:
-        "https://img01.ztat.net/article/spp-media-p1/6ffb430c75684971bedcb5780812b39b/250d192186204b40af5a3dde6b8638fa.jpg?imwidth=1800&filter=packshot",
-      imgAlt: "sneakers 1",
-    },
-    {
-      imgSrc:
-        "https://img01.ztat.net/article/spp-media-p1/9493dcbd4ba34f248970c71e23bdf411/31056d77f6334644a49e3328684b0bd0.jpg?imwidth=1800",
-      imgAlt: "sneakers 2",
-    },
-    {
-      imgSrc:
-        "https://img01.ztat.net/article/spp-media-p1/d1af88562d56401badd0c6bbe1cdc82b/6ba23cbb61154a3997544bec8543a059.jpg?imwidth=1800",
-      imgAlt: "sneakers 3",
-    },
-    {
-      imgSrc:
-        "https://img01.ztat.net/article/spp-media-p1/0c139df2194a402794d654ab29627455/bf96e816d9eb49748fe7491d5f597468.jpg?imwidth=1800",
-      imgAlt: "sneakers 4",
-    },
-  ];
-
   return (
     <>
       <NavBar />
       <main>
         <PicturesShoes />
+        <DescriptionShoes />
       </main>
     </>
   );
--- src/components/PicturesShoes.jsx
+++ src/components/PicturesShoes.jsx
@@ -101,38 +101,6 @@ function PicturesShoes() {
           </ul>
         </section>
       </section>
-      <section className="sectionRight">
-        <p className="brand">SNEAKER COMPANY</p>
-        <h2>Fall Limited Edition Sneakers</h2>
-        <p className="description">
-          These low-profile sneakers are your perfect casual wear companion.
-          Featuring a durable rubber outer sole, they'll withstand everyting the
-          weather can offer.
-        </p>
-        <section className="reductionPrice">
-          <p className="price">$125.00</p>
-          <p className="reduction">50%</p>
-        </section>
-        <p className="oldPrice">$250.00</p>
-        <section className="purchase">
-          <div className="quantity">
-            <button type="button" className="less" onClick={handleClickLess}>
-              -
-            </button>
-            {quantity < 0 ? (
-              <p className="numberOfQuantity">0</p>
-            ) : (
-              <p className="numberOfQuantity">{quantity}</p>
-            )}
-            <button type="button" className="more" onClick={handleClickMore}>
-              +
-            </button>
-          </div>
-          <button type="button" className="addToCart">
-            🛒 Add to cart
-          </button>
-        </section>
-      </section>
     </>
   );
 }
